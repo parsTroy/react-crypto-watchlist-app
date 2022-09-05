45babf53fee5b83f3df11d701486e519e13cdf92
parsTroy <troyparsons@hotmail.com>
2022-09-05 13:25:01 -0400

    update functionality for forms and accounts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Signup from './routes/Signup';
 import Account from './routes/Account';
 import axios from 'axios'
 import CoinPage from './routes/CoinPage';
+import Footer from './components/Footer';
+import { AuthContextProvider } from './context/AuthContext';
 
 function App() {
   const [coins, setCoins] = useState([])
@@ -23,15 +25,18 @@ function App() {
 
   return (
     <ThemeProvider>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home coins={coins} />} />
-        <Route path='/signin' element={<Signin />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/account' element={<Account />} />
-        <Route path='/coin/:coinId' element={<CoinPage />} />
-        <Route path=':coinId' />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home coins={coins} />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/coin/:coinId' element={<CoinPage />} />
+          <Route path=':coinId' />
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
